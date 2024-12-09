80df5288ca28690166aa9f3723e141f041fb2705
Malay Ladu <malayladu@gmail.com>
2024-12-09 15:23:45 +0530

    Update workdlow

--- blocksy.php
+++ blocksy.php
@@ -3,14 +3,14 @@
 
 
 /**
- * @TODO: Test Issue #1
+ * TODO: Test Issue #1
  *
  * This is a test issue to see if the issue is created by the workflow.
  */
 
 
 /**
- * @TODO: Test Issue #2
+ * TODO: Test Issue #2
  *
  * This is a test issue to see if the issue is created by the workflow.
  */
