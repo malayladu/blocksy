4311699750225bb68643372750673240bfe377d0
Malay Ladu <malayladu@gmail.com>
2025-02-07 23:52:19 +0530

    Update

--- blocksy.php
+++ blocksy.php
@@ -1,16 +1,21 @@
 <?php
-
-
-
 /**
- * TODO: Test Issue #1
- *
- * This is a test issue to see if the issue is created by the workflow.
+ * Plugin Name: Blocksy
+ * Plugin URI: https://malayladu.com/
+ * Description: Test plugin for Blocksy theme.
+ * Version: 1.0
+ * Author: Malay Ladu
+ * Author URI: http://malayladu.com/
+ * Text Domain: blocksy
+ * Domain Path: /languages
+ * License: GPL2
  */
 
 
-/**
- * TODO: Test Issue #2
- *
- * This is a test issue to see if the issue is created by the workflow.
- */
+load_plugin_textdomain(
+	'blocksy',
+	false,
+	dirname( dirname( \plugin_basename( __FILE__ ) ) ) . '/languages/'
+);
+
+
--- includes/blocks/copyright-date-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '74f7c4e5ffdf1475c2aa');
